f146e2cee6a2d190512e8a9a4c0d49062cdb877f
Rayiz3 <jusmint3@gmail.com>
2024-12-30 00:07:35 +0900

    add : roomDB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -53,4 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler) // For annotation processing
+    implementation(libs.androidx.room.ktx) // For Kotlin coroutine support
 }
