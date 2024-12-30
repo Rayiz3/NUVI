e91f8d6056e1dcf833cb6040bea0cacebd4ceeb9
dojmj <dojmj@naver.com>
2024-12-30 15:36:00 +0900

    contact 제목 수정

--- app/src/main/java/com/example/myapplication/ui/contact/ContactFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/contact/ContactFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentContactBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -23,6 +25,12 @@ class ContactFragment : Fragment() {
 
         setUpFirstRecyclerView()
         setUpSecondRecyclerView()
+
+        val textTitleView: TextView = binding.contactTitle
+        val textSubtitleView: TextView = binding.contactSubtitle
+
+        textTitleView.text = getString(R.string.title_contact)
+        textSubtitleView.text = getString(R.string.subtitle_contact)
 
         return binding.root
     }
